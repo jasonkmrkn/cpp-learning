f89c84fac09146ccb8b2cfb7f5f3a6dd9d7f8f57
jasonkmrkn <89698380+jasonkmrkn@users.noreply.github.com>
2022-02-12 14:32:35 +0700

    Edited and Added utils to multifile_compilation

--- calcur-course/exercise/multifile_compilation/main.cpp
+++ calcur-course/exercise/multifile_compilation/main.cpp
@@ -24,3 +24,15 @@ double pow(double base, int power = 2){
     }
     return total;
 }
+
+int main() {
+      std::cout << pow(3,3) << std::endl;
+      std::cout << pow(3) << std::endl;
+//    Rectangle rectangle;
+//    rectangle.length = 10;
+//    rectangle.width = 10;
+//    std::cout << area(rectangle.length, rectangle.width) << "\n"; //this function call will refer to the area() function that needs 2 arguments.
+//    std::cout << area(rectangle.length) << "\n"; // this function call will refer to the area() function that only need one argument.
+//    std::cout << area(rectangle) << "\n"; // this function call will refer to the area() function that need the "Rectangle" object.
+    return 0;
+}
