05dbc595eab760a4d984d025d2fbacbdee25d522
jasonkmrkn <89698380+jasonkmrkn@users.noreply.github.com>
2022-02-12 14:20:42 +0700

    Added main.cpp to multifile_compilation

---
+++ calcur-course/exercise/multifile_compilation/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+struct Rectangle{
+    double length;
+    double width;
+};
+
+double area(double length, double width){
+    return length*width;
+}
+
+double area(double length){
+    return length*length;
+}
+
+double area(Rectangle rectangle){
+    return rectangle.length*rectangle.width;
+}
+
+double pow(double base, int power = 2){
+    int total = 1;
+    for(int i = 0; i < power; i++){
+        total *= base;
+    }
+    return total;
+}
