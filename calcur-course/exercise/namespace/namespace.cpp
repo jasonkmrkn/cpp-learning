943786f08a4fb850e40ef9688ee457ddd3eaaa08
jasonkmrkn <89698380+jasonkmrkn@users.noreply.github.com>
2022-02-15 12:11:10 +0700

    Added namespace folder

---
+++ calcur-course/exercise/namespace/namespace.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+void printArray(const int data[], int size){
+    for (int i = 0; i < size; i++) {
+        std::cout << data[i] << "\t";
+    }
+    std::cout << "\n";
+}
+
+int main(){
+    int data[] = {1, 2, 3};
+    printArray(data);
+}
---
+++ calcur-course/exercise/namespace/namespace.md
@@ -0,0 +1,12 @@
+To make our own namespace 
+``
+namespace utils{
+//part of the namespace
+}
+``
+
+why we use namespace? 
+1. Common functionality
+2. a particular project
+3. classifying / categories
+
